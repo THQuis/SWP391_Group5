5c067a457d3d02288863f8f3cbb1ea188efcb144
Miltt4869 <milttse184379@fpt.edu.vn>
2025-06-08 00:56:17 +0700

    Add files via upload

---
+++ Frontend/Code/admin/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import './App.css'; // Custom CSS
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);

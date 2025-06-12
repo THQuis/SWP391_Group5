5cc70844036d3150784392b161488e08aec7782f
Miltt4869 <milttse184379@fpt.edu.vn>
2025-06-12 21:15:26 +0700

    chuyển từ branch qua main

---
+++ Frontend/smokingplatformReact/src/App.js
@@ -0,0 +1,9 @@
+import RouterCustom from './routes/RouterCustom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return <RouterCustom />;
+
+}
+
+export default App;
---
+++ Frontend/smokingplatformReact/src/utils/router.js
@@ -0,0 +1,20 @@
+export const ROUTERS = {
+    AUTH: {
+        LOGIN: "/auth",
+    },
+    USER: {
+        HOME: "/",
+        HOME2: "/"
+    },
+    ADMIN: {
+        DASHBOARD: "/admin",
+        MANAUSER: "/admin/user",
+        MANAPACKAGE: "/admin/package",
+        MANABLOG: "/admin/blog",
+        MANAPERFORMANCE: "/admin/performance",
+        MANANOTIFICATION: "/admin/notification",
+        MANAPLAN: "/admin/plan",
+
+
+    },
+};

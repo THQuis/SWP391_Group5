759f53b3d0aaa8da43f5394e866fcb8af311c8fe
Trương Hoàng Quí <quithse184355@fpt.edu.vn>
2025-06-13 10:35:33 +0700

    Delete BLL directory

--- BLL/Interfaces/IMailService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Smoking.BLL.Interfaces
-{
-    public interface IMailService
-    {
-        Task SendOtpEmailAsync(string toEmail, string otpCode); // Cũ
-        Task SendHtmlEmailAsync(string toEmail, string subject, string htmlBody); // NEW → dùng cho Quên mật khẩu
-    }
-}

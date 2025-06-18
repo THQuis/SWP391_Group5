6b6add0d2886e4b45001611d0d8abac45f4b9411
Hữu Trí <caohuutritl1234@gmail.com>
2025-06-18 16:03:59 +0700

    Cập nhật

--- BE/Tri/API/obj/Debug/net9.0/Smoking.API.AssemblyInfo.cs
+++ BE/Tri/API/obj/Debug/net9.0/Smoking.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Smoking.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0a31e59e5dbd8f4439ada1dd2c8fdb3ddfabf1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+947f3c78263691f9acc13b7c422aa6ae36f27c50")]
 [assembly: System.Reflection.AssemblyProductAttribute("Smoking.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Smoking.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

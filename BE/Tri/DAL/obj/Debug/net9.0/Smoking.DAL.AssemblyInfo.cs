1e112944b8032f48942b956b4839463a1ea42bf9
Miltt4869 <milttse184379@fpt.edu.vn>
2025-06-23 21:22:33 +0700

    them phan phan goi cho mock

--- BE/Tri/DAL/obj/Debug/net9.0/Smoking.DAL.AssemblyInfo.cs
+++ BE/Tri/DAL/obj/Debug/net9.0/Smoking.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Smoking.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+095f6a52cc4efce950352e326333c543d68b0917")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70b42cd0bafb4e149bea20f90c8aa6db2539776e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Smoking.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Smoking.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

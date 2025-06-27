617ddc4df9f7f925dbb22d1221b9f220d37c6f19
Hữu Trí <caohuutritl1234@gmail.com>
2025-06-27 21:18:43 +0700

    Tao up lên nhé Quí lỏ

--- BE/DAL/obj/Debug/net9.0/Smoking.DAL.AssemblyInfo.cs
+++ BE/DAL/obj/Debug/net9.0/Smoking.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Smoking.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c89b85d8784143ab905a53112983eecfbce869c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62fa909628fbc90f7c033816ca5eda2b3f0d214f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Smoking.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Smoking.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

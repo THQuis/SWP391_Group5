f306f7c26147aefecba5706bf3078e2d82b9a90f
S-bon-debug <sonngocson25@gmail.com>
2025-06-30 17:17:48 +0700

    xong qui 2

--- BE/BLL/Interfaces/IMailService.cs
+++ BE/BLL/Interfaces/IMailService.cs
@@ -4,15 +4,10 @@ namespace Smoking.BLL.Interfaces
 {
     public interface IMailService
     {
-<<<<<<< HEAD
+
         Task SendOtpEmailAsync(string toEmail, string otpCode); // Cũ
         Task SendHtmlEmailAsync(string toEmail, string subject, string htmlBody); // NEW → dùng cho Quên mật khẩu
         Task SendEmailAsync(string toEmail, string subject, string body); // Để gửi email cơ bản
 
-=======
-        Task SendOtpEmailAsync(string toEmail, string otpCode); 
-        Task SendHtmlEmailAsync(string toEmail, string subject, string htmlBody);
-        Task SendEmailAsync(string toEmail, string subject, string body); 
->>>>>>> 277113ebafa3c7c1d7d64e1dcda6049f3a9e535e
     }
 }

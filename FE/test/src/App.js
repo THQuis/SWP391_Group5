f1cbf12f80d1814590ed23eb6830b2d7bc62853a
THQuis <quithse184355@fpt.edu.vn>
2025-07-02 14:34:33 +0700

    build12

--- FE/test/src/App.js
+++ FE/test/src/App.js
@@ -2,6 +2,7 @@ import RouterCustom from './routes/RouterCustom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 function App() {
   return (
     <div>
@@ -21,6 +22,7 @@ function App() {
         pauseOnHover
         theme="light" // hoặc "dark", "colored"
       />
+      <SpeedInsights />
     </div>
   );
 }

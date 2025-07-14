40a4ba64b03f89ebed5c24c038e9b9115778f4c0
Hữu Trí <caohuutritl1234@gmail.com>
2025-07-14 15:10:53 +0700

    New

---
+++ BE/DAL/Entities/BlogReaction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Smoking.DAL.Entities
+{
+    public class BlogReaction
+    {
+        public int Id { get; set; }
+        public int BlogId { get; set; }
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// TRUE = Like, FALSE = Dislike, NULL = Không phản ứng
+        /// </summary>
+        public bool? IsLike { get; set; }
+
+        public DateTime? ReactedAt { get; set; }
+
+        // Navigation properties
+        public virtual Blog Blog { get; set; }
+        public virtual User User { get; set; }
+    }
+}
